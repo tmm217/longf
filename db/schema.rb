766e1e319bb81fee1b2bad8585fdc78e1a0135b4
tmm217 <tmattera@chicagobooth.edu>
2022-01-17 10:22:09 -0500

    updated homepage logic with article address

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_17_144220) do
+ActiveRecord::Schema.define(version: 2022_01_17_151709) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2022_01_17_144220) do
     t.integer "pub_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "address"
   end
 
   create_table "publications", force: :cascade do |t|
